ab8641d88d83ba81d9bee08fa7f52b4c741e29ab
Silvia <sarnaizgarcia@gmail.com>
2021-02-13 09:51:36 +0100

    Soluciono el conflicto (ahora sí)

--- src/principal/Cociente.java
+++ src/principal/Cociente.java
@@ -1,12 +1,5 @@
 package principal;
 
-<<<<<<< HEAD
-import java.util.*;
-
-/**
- * @author sarnaizgarcia
- * Esta clase implementa los metodos relacionados con la division de la clase Calculadora
-=======
 import java.lang.Math;
 
 
@@ -15,7 +8,6 @@ import java.lang.Math;
  * la <b>división</b> de la clase Calculadora.
  * 
  * @author sarnaizgarcia. 
->>>>>>> cociente
  */
 
 public class Cociente {
@@ -117,57 +109,4 @@ public class Cociente {
 		return result;
 	}
 
-<<<<<<< HEAD
-	/**
-	 * Este metodo divide dos numeros enteros
-	 * 
-	 * @param f1 dividendo real
-	 * @param f2 divisor real
-	 * 
-	 * @return division de f1/f2
-	 */
-
-	public float cocienteReales(float f1, float f2) {
-		return (f1/f2);
-	}
-
-	/**
-	 * Este metodo divide dos numeros reales
-	 * 
-	 * @param n1 dividendo entero
-	 * @param n2 divisor entero
-	 * 
-	 * @return division de n1/n2
-	 */
-
-	public int cocienteEnteros(int n1, int n2) {
-		return (n1/n2);
-	}
-
-	/**
-	 * Este metodo calcula el inverso de un numero
-	 * 
-	 * @param x
-	 * 
-	 * @return inverso de x
-	 */
-
-	public float inverso(float x) {
-		return (1/x);
-	}
-
-	/**
-	 * Este metodo calcula la raiz cuadrada de un numero
-	 * 
-	 * @param y
-	 * 
-	 * @return raiz cuadrada de y
-	 */
-
-	public double raiz(int y) {
-		return Math.sqrt(y);
-	}
-
-=======
->>>>>>> cociente
 }
