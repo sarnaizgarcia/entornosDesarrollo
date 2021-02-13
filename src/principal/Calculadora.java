04156167341f7adb140c892c658dadeb332be51a
MerySagra <mariajsm5@hotmail.com>
2021-02-13 10:35:26 +0100

    Añado los métodos de la clase resta en Calculadora

--- src/principal/Calculadora.java
+++ src/principal/Calculadora.java
@@ -3,6 +3,14 @@ package principal;
 public class Calculadora {
 
 	public static void main(String[] args) {
+		
+		/**
+		 * Creamos un objeto <b> cociente </b> para utilizar los métodos de la clase.
+		 * @author sarnaizgarcia.
+		 * @version 2.2
+		 * @since 13/02/2021
+		 */
+		
 		Cociente c = new Cociente();
 
 		double divReales = c.cocienteReales(10.5F, 3.5F);
@@ -14,6 +22,22 @@ public class Calculadora {
 		System.out.println(divEnteros);
 		System.out.println(inv);
 		System.out.println(raizCuadrada);
+		
+		/**
+		 * Creamos un objeto <b> resta </b> para utilizar los métodos de la clase.
+		 * @author MerySagra
+		 * @version 2.2
+		 * @since 13/02/2021
+		 */
+		
+		Resta r = new Resta();
+		
+		System.out.println(r.restaReales(50.6, 20.1));
+		System.out.println(r.restaEnteros(70, 5));
+		System.out.println(r.restaTresReales(70, 5, 12));
+		System.out.println(r.acumulado(16));
+		
+		
 	}
 
 }
